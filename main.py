8cdab9df1aff0f04e9f34c4459b104a8bf77dc4e
Shivam Pokhriyal <shivampokhriyal1997@gmail.com>
2024-08-12 00:58:55 +0530

    adding cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
-
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # List your allowed origins here
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify allowed methods, or use ["*"] to allow all
+    allow_headers=["*"],  # You can specify allowed headers, or use ["*"] to allow all
+)
 
 @app.get("/")
 async def root():
