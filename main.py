d7c6055910183522e722ee014b8cf0ac0a515968
Shivam Pokhriyal <shivampokhriyal1997@gmail.com>
2024-08-12 19:40:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ app = FastAPI()
 
 
 CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",  
-    "Access-Control-Allow-Methods": "*", 
-    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*", 
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Credentials": "true"
 }
 
